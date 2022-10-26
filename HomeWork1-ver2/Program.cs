3e47834377afc317f8281807a9e091c0c9891d90
Dmitry Nagovitsyn <nagovicinde@mail.ru>
2022-10-26 23:47:10 +0400

    HomeWork1-ver2 added

---
+++ HomeWork1-ver2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//  Разделить ввод данных, от расчета данных 
+//  (Одна функция вводит данные, другая функция подсчитывает количество положительных)
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Сколько цифр Вы хотите ввести? :");
+int chislo = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArray (int chislo)
+{
+    int length = chislo;
+    int[] massiv = new int[length];
+    for (int i = 0; i <= massiv.Length - 1; i++)
+    {
+        System.Console.Write($"Введите число {i+1}: ");
+        massiv[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return massiv;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int FindPolozitelnie(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] massiv = CreateArray(chislo);
+PrintArray(massiv);
+System.Console.WriteLine($"Среди введенных чисел {FindPolozitelnie(massiv)} больше <0>");
